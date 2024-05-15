f7975e538718e6c236b67ab538339fe541536afd
Christopher Duarte <christopher.duarte@middletownct.gov>
2024-05-15 15:43:07 -0400

    WIP

--- src/Module.php
+++ src/Module.php
@@ -8,8 +8,6 @@ final class Module
     public function getConfig(): array
     {
         $provider = new ConfigProvider();
-        return [
-            'service_manager' => $provider->getDependencyConfig(),
-        ];
+        return $provider->getDependencyConfig();
     }
 }
