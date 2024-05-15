155b17f45441a8f11fda12589e7849e8b254eaa9
Christopher Duarte <christopher.duarte@middletownct.gov>
2024-05-15 14:30:31 -0400

    WIP

---
+++ src/View/Helper/Factory/FormCheckboxFactory.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Dassociates\Bootstrap\View\Helper\Factory;
+
+use Laminas\Form\View\Helper\FormElement;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+use Dassociates\Html\View\Helper\FormCheckbox;
+
+class FormCheckboxFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
+    {
+        /**
+         * 
+         * @var FormElement $view_helper
+         */
+        $view_helper = $container->get('ViewHelperManager')->get(FormElement::class);
+        $view_helper->addType('checkbox', FormCheckbox::class);
+        return null === $options ? new $requestedName() : new $requestedName($options);
+    }
+}
